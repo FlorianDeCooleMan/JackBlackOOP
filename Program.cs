471bae476207bf2c0644362cc2a32a345c335194
FlorianDeCooleMan <120579373+FlorianDeCooleMan@users.noreply.github.com>
2024-02-28 13:06:45 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace JackBlackOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
